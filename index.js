2dea1ef4124cc6614a05a6229d2b7aff73abf968
Vedant Pandya <vedant.pandya@avestatechnologies.com>
2024-02-15 14:59:00 +0530

    added 2 new apis in items

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const app = express()
-const login = require('./login/login.route')
-const bodyparser = require('body-parser')
-const register = require('./register')
-const users = require('./users')
-const { ValidationError } = require('express-validation')
-const cors = require('cors')
-const items = require('./items')
-app.use(cors({origin : 'http://localhost:3000'}))
-app.use(bodyparser.json())
-app.use(express.urlencoded({
-    extended: false
-  }));
-
-  app.use('/login',login)
-app.use('/register' , register)
-app.use('/user' , users)
-app.use('/item',items)
-app.use(function(err, req, res, next) {
-    if (err instanceof ValidationError) {
-      return res.status(err.statusCode).json(err.details)
-    }
-    return res.status(500).json(err)
+const express = require("express");
+const app = express();
+const login = require("./login/login.route");
+const bodyparser = require("body-parser");
+const register = require("./register");
+const users = require("./users");
+const { ValidationError } = require("express-validation");
+const cors = require("cors");
+const items = require("./items");
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(bodyparser.json());
+app.use(
+  express.urlencoded({
+    extended: false,
   })
+);
 
-app.listen(3030)
+app.use("/login", login);
+app.use("/register", register);
+app.use("/user", users);
+app.use("/item", items);
+app.use(function (err, req, res, next) {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json(err.details);
+  }
+  return res.status(500).json(err);
+});
+
+app.listen(3030);
--- items.js
+++ items.js
@@ -6,11 +6,22 @@ router.get('/', async(req,res)=> {
     res.json(rows)
 }) 
 
-router.post('/add',async(req,res) => {
+router.post('/',async(req,res) => {
     const { name , category , price , hotel_id } = req.body
     const {rows} = await client.query('insert into items(name , category , price ,hotel_id ) values($1,$2,$3,$4) returning id',[name , category , price , hotel_id])
     const response = await client.query('select * from items where id = $1' ,[rows[0].id])
     res.json(response.rows)
+})
+
+router.put('/:id' , async (req,res) => {
+    let item = req.body
+    const {rows} = await client.query('update items set name = $1 and category = $2 and price = $3 where id = $4',[item.name , item.category , item.price ,req.params.id])
+    res.json({message : 'done'})
+})
+
+router.delete('/:id' , async (req,res) => {
+    const {rows} = await client.query('delete from items where id = $1',[req.params.id])
+    res.json({message : 'done'})
 })
 
 module.exports = router
