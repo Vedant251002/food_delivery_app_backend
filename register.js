d968acd7ab52ca8010789a0b5da460ea2b5cc466
Vedant Pandya <vedant.pandya@avestatechnologies.com>
2024-02-15 17:07:45 +0530

    added role changes in register api

--- register.js
+++ register.js
@@ -9,13 +9,14 @@ const validateUser = {
         password : Joi.string().required(),
         res_name : Joi.string().required(),
         address : Joi.string().required(),
-        mobileno : Joi.string().required().max(10)
+        mobileno : Joi.string().required().max(10),
+        role : Joi.string().required().valid('admin','customer'),
     })
 }
 
 router.post('/' , validate(validateUser),async(req,res) => {
-    let {name , password } = req.body
-    let {rows} = await client.query('insert into users(name,password)values($1,$2) returning id',[name ,password])
+    let {name , password , role } = req.body
+    let {rows} = await client.query('insert into users(name,password,role)values($1,$2,$3) returning id',[name ,password , role])
     let {res_name , address , mobileno} = req.body
     let data = await client.query('insert into restaurants(name,address,mobileno,owner)values($1,$2,$3,$4)',[res_name,address,mobileno,rows[0].id])
     res.json({message : 'done'})
--- users.js
+++ users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const client = require('./config')
 router.get('/' , async(req,res) => {
-    let {rows} = await client.query('select id,name from users where id = $1',[req.query.id])
+    let {rows} = await client.query('select id,name , role from users where id = $1',[req.query.id])
     res.json(rows)
 })
 
